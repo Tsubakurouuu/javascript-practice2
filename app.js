70dc010e825918245f23f89759b3c9853641979d
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-13 00:44:21 +0000

    J スコープ　巻き上げ

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
+var str = "webcamp"
+
 function foo() {
-  let x = "webcamp"
-  {
-    let y = "hello webcamp"
-  }
-  console.log(x)
-  console.log(y)
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
 }
 
 foo()
-
-for (let i = 0; i < 10; i++) {
-  console.log(i)
-}
-
-console.log(i)
